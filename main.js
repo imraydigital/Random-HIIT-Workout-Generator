bf64dc9186a19ac9dbe836367153607fd46122e6
Liam Imray <liam@liamimrayfitness.com>
2021-10-12 08:56:53 +0100

    Terminal Only Version

---
+++ main.js
@@ -0,0 +1,17 @@
+// Create options object
+const options = {
+  exercise: ["Squats", "Lunges", "Press Ups", "Crunches", "Burpees", "Squat Jumps", "Bicycle Crunches", "Sprint on spot", "Jumping Jacks", "Plank", "Plank Walkouts", "Mountain Climbers", "Squat Thrusts", "Lunge Jumps", "Jacknives", "Lunge Walks"],
+  duration: [30, 45, 60],
+  rest: [10, 20, 30],
+  randNum(prop) {
+    return Math.floor(Math.random() * prop.length)
+  },
+  generateExercise() {
+    const randDuration = this.duration[this.randNum(this.duration)];
+    const randExercise = this.exercise[this.randNum(this.exercise)];
+    const randRest = this.rest[this.randNum(this.rest)];
+    return `Do ${randExercise} for ${randDuration} seconds.\nThen rest for ${randRest} seconds.`;
+  }
+}
+// Log random exercise to console
+console.log(options.generateExercise());
